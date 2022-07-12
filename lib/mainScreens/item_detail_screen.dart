36e913b9490b5dd482d4fc6526a46c93a7768258
quangkhatran <quangkhatran@gmail.com>
2022-07-13 05:23:58 +0700

    implement Food Items Increment, Decrement Counter

--- lib/mainScreens/item_detail_screen.dart
+++ lib/mainScreens/item_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:number_inc_dec/number_inc_dec.dart';
 
 import '../models/items.dart';
 import '../widgets/app_bar.dart';
@@ -12,10 +13,87 @@ class ItemDetailsScreen extends StatefulWidget {
 }
 
 class _ItemDetailsScreenState extends State<ItemDetailsScreen> {
+  TextEditingController counterTextEditingController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: MyAppBar(),
+      body: Column(
+        children: [
+          Image.network(widget.model!.thumbnailUrl.toString()),
+          Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: NumberInputPrefabbed.roundedButtons(
+              controller: counterTextEditingController,
+              incDecBgColor: Colors.amber,
+              min: 1,
+              max: 9,
+              initialValue: 1,
+              buttonArrangement: ButtonArrangement.incRightDecLeft,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              widget.model!.title.toString(),
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              widget.model!.longDescription.toString(),
+              textAlign: TextAlign.justify,
+              style: const TextStyle(
+                fontWeight: FontWeight.normal,
+                fontSize: 14,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              '£' + widget.model!.price.toString(),
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Center(
+            child: InkWell(
+                onTap: () {},
+                child: Container(
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.cyan,
+                        Colors.amber,
+                      ],
+                      begin: FractionalOffset(0.0, 0.0),
+                      end: FractionalOffset(1.0, 0.0),
+                      stops: [0.0, 1.0],
+                      tileMode: TileMode.clamp,
+                    ),
+                  ),
+                  width: MediaQuery.of(context).size.width - 13,
+                  height: 50,
+                  child: Center(
+                    child: Text(
+                      'Add to Cart',
+                      style: TextStyle(color: Colors.white, fontSize: 15),
+                    ),
+                  ),
+                )),
+          ),
+        ],
+      ),
     );
   }
 }
